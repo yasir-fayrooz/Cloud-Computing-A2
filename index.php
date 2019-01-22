89fde470a0af8709596fab8c3c2f60126b91e250
Yasir Fayrooz <30676134+yasir-fayrooz@users.noreply.github.com>
2019-01-22 16:13:24 +1100

    started project
    implemented basic home page

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+?>
+<html>
+ <body>  
+ <center>
+  <div><h1>Enter Steam ID:</h1></div>
+  <input name="home" href="index.php">
+ <form method="post">
+  <div><input name="steam_ID"></input> <input type="submit" value="Submit"></div>
+ </center>
+ </form>
+</body>
+</html>
