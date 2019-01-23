cc93b5dbbb3f153cce2766b47c70922889bbace7
Yasir Fayrooz <30676134+yasir-fayrooz@users.noreply.github.com>
2019-01-23 15:49:09 +1100

    changed some text

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <html>
  <body>  
  <center>
-  <div><h1>Enter Steam ID:</h1></div>
+  <div><h1>Home:</h1></div>
    <a href="index.php">Home</a>    <a href="steam.php">Steam</a>    <a href="twitch.php">Twitch</a> 
  </center>
 </body>
