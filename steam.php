1060528c1ed35f339d3635566594f417e09571cc
Yasir Fayrooz <30676134+yasir-fayrooz@users.noreply.github.com>
2019-01-23 15:44:09 +1100

    added pages

--- steam.php
+++ steam.php
@@ -6,6 +6,10 @@
  <center>
   <div><h1>Enter Steam ID:</h1></div>
    <a href="index.php">Home</a>    <a href="steam.php">Steam</a>    <a href="twitch.php">Twitch</a> 
+  <input name="home" href="index.php">
+ <form method="post">
+  <div><input name="steam_ID"></input> <input type="submit" value="Submit"></div>
  </center>
+ </form>
 </body>
 </html>
--- twitch.php
+++ twitch.php
@@ -5,7 +5,7 @@
  <body>  
  <center>
   <div><h1>Enter Steam ID:</h1></div>
-   <a href="index.php">Home</a>    <a href="steam.php">Steam</a>    <a href="twitch.php">Twitch</a> 
+   <a href="index.php">Home</a>    <a href="steam.php">Steam</a>    <a href="twitch.php">Twitch</a>
  </center>
 </body>
 </html>
