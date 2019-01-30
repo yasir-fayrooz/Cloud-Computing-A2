f7bcd281163beed58d4cd827dee6b9e458c4d333
Yasir Fayrooz <30676134+yasir-fayrooz@users.noreply.github.com>
2019-01-30 12:55:40 +1100

    Added a table with stored datastore info

--- queryapi.php
+++ queryapi.php
@@ -1,29 +1,44 @@
 <?php
-	
 	# Includes the autoloader for libraries installed with composer
 	require __DIR__ . '/vendor/autoload.php';
 	# Imports the Google Cloud client library
 	use Google\Cloud\Datastore\DatastoreClient;
 	
-	$datastore = new DatastoreClient([
-    'projectId' => 'my_project'
-	]);
+	# Your Google Cloud Platform project ID
+	$projectId = 'cc-steamapi';
 
+	# Instantiates a client
+	$datastore = new DatastoreClient(['projectId' => $projectId]);
 
 	$query = $datastore->query();
-	$query->kind('Notes');
-	$query->filter('name ', '=', 'test1');
+	$query->kind('SteamDB');
 
-	$res = $datastore->runQuery($query);
-	foreach ($res as $notes) {
-		echo $notes['name']; // test1
-	}
+	$result = $datastore->runQuery($query);
 ?>
 <html>
  <body>
  <center>
   <div><h1>Steam Queries:</h1></div>
-     <a href="index.php">Home</a>    <a href="steam.php">Steam</a>    <a href="twitch.php">Twitch</a> <a href="queryapi.php">Steam Queries</a> 
+     <a href="index.php">Home</a>    <a href="steam.php">Steam</a>    <a href="twitch.php">Twitch</a> <a href="queryapi.php">Steam Queries</a>
+  <table border="1" cellspacing="5">
+  <tr>
+  <td><b><center>SteamID64:</center></b></td>
+  <td><b><center>Display Name:</center></b></td>
+  <td><b><center>Steam Level:</center></b></td>
+  <td><b><center>Friends Count:</center></b></td>
+  <td><b><center>Game Count:</center></b></td>
+  <td><b><center>Hour Count:</center></b></td>
+  </tr>
+	<?php foreach ($result as $SteamDB): ?>
+	<tr>
+	<td><center><?php echo $SteamDB['SteamID64']; ?></center></td>
+	<td><center><?php echo $SteamDB['Display Name']; ?></center></td>
+	<td><center><?php echo $SteamDB['Steam Level']; ?></center></td>
+	<td><center><?php echo $SteamDB['Friends Count']; ?></center></td>
+	<td><center><?php echo $SteamDB['Game Count']; ?></center></td>
+	<td><center><?php echo $SteamDB['Hour Count']; ?></center></td>
+	</tr>
+	<?php endforeach; ?>
   </center> 
 </body>
 </html>
