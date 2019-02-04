2daf6c7319af9cc40045a1a5f0cdf410dfca5850
Jonathan <s3657271@student.rmit.edu.au>
2019-02-04 16:57:09 +1100

    fixing text

--- index.php
+++ index.php
@@ -36,16 +36,19 @@
 
     <p class="homeContent">Our application name 'AJAY' is an acronym, which is made up of the first letter of each team member's name. We collectively decided and agreed upon this name as it is a unique name. Our group name carries an important meaning, that being we recognise and highly value each team member because we all have something special to offer.</p>
     
-    
+    <br>
+    <br>
 <h1 class="steamError">What does our application do?</h1>    
     
     <p class="homeContent">Our application collects a range of Steam player information and stores it inside a database. To access this data, all the user needs to do is enter their Steam id and click the submit button, after which they'll have access to several types of information. This includes the number of hours played, number of friends etc. The user can also search for other player's Steam data too, provided that they enter a valid Steam name. In addition to the Steam page, there is also a Steam query page where the user can compare their data with other players. The user needs to simply select a criteria and how they want the data to be ordered. For instance, the user may select number of hours played as the criteria and choose to order the data from highest to lowest. After the user clicks submit button, the appropriate data is displayed in a table format which is easy to read.</p>
-    
+    <br>
+    <br>
 <h1 class="steamError">What were the motivations behind our idea?</h1>    
     
     <p class="homeContent">As all group members are passionate about games, we decided as a group, it would be an interesting idea if we were to collect and analyse some player data from Steam. Steam is a popular platform for pc gamers, which distributes a large variety of online games for people to download on their laptops/desktops and play. A mutual interest in games/gaming really helped us in making our final decision on what we wanted to do.</p>
     
-    
+    <br>
+    <br>
 <h1 class="steamError">Why should you use our application?</h1> 
     
     <p class="homeContent">There are many positives associated with our application. First of all, it's extremely easy to use and understand with its simple layout. Secondly, our application is free to use, so the thought of do I have to pay to use this, is instantly removed. Thirdly, our web application contains a Steam query page, where the user can compare their data with other Steam players. We added this feature because  there's a lot of competitive Steam players out there, who really care about all the little aspects. In summary, our team designed the application to be easy to use, free and fun for all audiences, from the casual players to the hardcore gamers.</p>
